9628fee063625199a5279f43592811107cb93273
guoxiao12138 <1761010152@qq.com>
2021-02-10 18:04:56 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/guoxiao/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.guoxiao.git;
+
+/**
+ * @author shkstart
+ * @create 2021-02-10-下午 5:52
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
